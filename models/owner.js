333065364859ed4fc4cd627efb069bfb82318145
dalhun namgung <bnamgung@gmail.com>
2017-12-23 12:39:01 -0800

    editing

---
+++ models/owner.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+
+const mongoose = require('mongoose');
+const Dog = require('./dog.js');
+const ownerSchema = new mongoose.Schema({
+  name: {type: String, required: true, unique: true},
+  dogName: {type: String},
+  dog: {type: mongoose.Schema.Types.ObjectId, ref: 'Dog'},
+  birthday: {type: Date, default: Date.now},
+});
+
+
+
+ownerSchema.pre('save', function(done) {
+  Dog.find({name: this.dogName})
+
+  .then(dogs => {
+    let dog = dogs[0];
+
+    if(! dog._id) {
+      throw new Error('owner error');
+    }
+    this.dog = dog._id;
+    done();
+  })
+  .then( () => done())
+  .catch(err => {console.log('this is err' + err);
+  done();
+});
+});
+
+
+ownerSchema.pre('findOne', function(done) {
+  this.populate({
+    path: 'dogs',
+  });
+  done();
+});
+
+
+
+module.exports = mongoose.model('Owner', ownerSchema);
